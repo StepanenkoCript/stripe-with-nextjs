c3754658eab5e4796ae6e582f383bf3ae12c6728
Bruno H Stepanenko <bruno@stepanenko.io>
2023-06-17 01:19:30 -0300

    img logo path fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/Images/Potion_craft_logo.png"
+          src="/checkout-project/public/images/potion_craft_logo.png"
           alt="potion craft Logo"
           width={180}
           height={37}
@@ -29,7 +29,7 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <Image src="/images/potion_green.png" alt='potion' width={200} height={200}/>
+          <Image src="/images/potion_green.PNG" alt='potion' width={200} height={200}/>
           <button className="group rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
           onClick={(() => {
             checkout({
